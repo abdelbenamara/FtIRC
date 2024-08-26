8d24485bf095477eae963083066412885b4a0ffc
Abdel Benamara <abdelb.contact@gmail.com>
2024-08-26 12:53:16 +0200

    Added Message

---
+++ Message.cpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Message.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/23 15:20:15 by abenamar          #+#    #+#             */
+/*   Updated: 2024/08/26 12:50:43 by abenamar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Message.hpp"
+
+std::string const Message::CRLF = "\r\n";
+
+std::size_t const Message::MAXSIZE = 512, Message::MAXCHARS = Message::MAXSIZE - Message::CRLF.length();
+
+char Message::BUFFER[Message::MAXSIZE];
+
+Message::Message(void) : input(), prefix(), command(), parameters() { return; }
+
+Message::Message(std::string const &input) : input(input), prefix(), command(), parameters() { return; }
+
+Message::Message(Message const &src) : input(src.input), prefix(src.prefix), command(src.command), parameters(src.parameters) { return; }
+
+Message::~Message(void) { return; }
+
+Message &Message::operator=(Message const & /* rhs */) throw() { return (*this); }
+
+std::string const &Message::getInput(void) const throw() { return (this->input); }
---
+++ Message.hpp
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Message.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/23 15:14:22 by abenamar          #+#    #+#             */
+/*   Updated: 2024/08/26 12:45:26 by abenamar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef __MESSAGE_HPP__
+#define __MESSAGE_HPP__
+
+#include <ostream>
+#include <stdexcept>
+#include <string>
+#include <vector>
+
+class Message
+{
+public:
+    class Builder
+    {
+    public:
+        Builder(void);
+        virtual ~Builder(void) throw();
+
+        Builder &withInput(std::string const &input);
+        Message build(void);
+
+    private:
+        std::string input, prefix, command;
+        std::vector<std::string> parameters;
+
+        Builder(Builder const & /* src */);
+
+        Builder &operator=(Builder const & /* rhs */) throw();
+    };
+
+    static std::string const CRLF;
+    static std::size_t const MAXSIZE, MAXCHARS;
+    static char BUFFER[];
+
+    Message(std::string const &input);
+    Message(Message const &src);
+    virtual ~Message(void);
+
+    std::string const &getInput(void) const throw();
+
+private:
+    std::string const input, prefix, command;
+    std::vector<std::string> const parameters;
+
+    Message(void);
+
+    Message &operator=(Message const & /* rhs */) throw();
+};
+
+#endif
