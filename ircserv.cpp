55e98d0d4ff332eaba1a9b3cb3b05f229a047b33
Abdel Benamara <abdelb.contact@gmail.com>
2024-08-26 12:55:00 +0200

    Updated ircserv

--- ircserv.cpp
+++ ircserv.cpp
@@ -6,20 +6,64 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/14 22:40:21 by abenamar          #+#    #+#             */
-/*   Updated: 2024/08/11 14:32:46 by abenamar         ###   ########.fr       */
+/*   Updated: 2024/08/26 12:22:33 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <csignal>
 #include <exception>
 #include <iostream>
+#include <stdexcept>
+#include "Client.hpp"
+#include "Message.hpp"
+#include "RuntimeErrno.hpp"
 #include "Server.hpp"
 
-void noop(int /* signum */) throw() { return; }
+static void noop(int /* signum */) throw() { return; }
+
+static void run(Server *const server) throw(std::runtime_error)
+{
+	std::map<int, Client *const>::const_iterator it;
+	int nfds, fd;
+
+	while (true)
+	{
+		nfds = server->waitForEvents();
+
+		try
+		{
+			for (int i = 0; i < nfds; ++i)
+			{
+				fd = server->getEventSocket(i);
+
+				if (fd == server->getSocket())
+					server->addClient();
+				else if (!server->getClient(fd)->appendInput(Message::BUFFER))
+					server->removeClient(fd);
+			}
+
+			for (it = server->getClientsBegin(); it != server->getClientsEnd(); ++it)
+			{
+				while (it->second->hasMessage())
+				{
+					std::cout << "Info: Client: " << it->second->message().getInput();
+
+					it->second->dropMessage();
+				}
+			}
+		}
+		catch (std::exception const &e)
+		{
+			std::cerr << "Error: " << e.what() << std::endl;
+		}
+	}
+
+	return;
+}
 
 int main(int argc, char *argv[])
 {
-	Server *server;
+	Server *server = NULL;
 
 	if (argc != 3)
 	{
@@ -30,29 +74,20 @@ int main(int argc, char *argv[])
 
 	try
 	{
-		server = (Server::Builder())
+		server = Server::Builder()
 					 .withNumericServ(argv[1])
 					 .withPassword(argv[2])
 					 .build();
-	}
-	catch (std::exception const &e)
-	{
-		std::cerr << "Error: " << e.what() << std::endl;
 
-		return (1);
-	}
-
-	try
-	{
 		if (std::signal(SIGINT, ::noop) == SIG_ERR)
-			throw std::runtime_error(std::string("std::runtime_error: std::signal (SIGINT): ") + strerror(errno));
+			throw RuntimeErrno("std::signal (SIGINT)");
 
 		if (std::signal(SIGQUIT, ::noop) == SIG_ERR)
-			throw std::runtime_error(std::string("std::runtime_error: std::signal (SIGQUIT): ") + strerror(errno));
+			throw RuntimeErrno("std::signal (SIGQUIT)");
 
 		std::cout << "Info: IRC server listening on port: " << server->port() << std::endl;
 
-		server->run();
+		::run(server);
 	}
 	catch (std::exception const &e)
 	{
