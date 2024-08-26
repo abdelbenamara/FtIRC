3b262327ac1acfb66e2931a78fd8fdc462f8ac12
Abdel Benamara <abdelb.contact@gmail.com>
2024-08-26 12:54:13 +0200

    Updated Client

--- Client.cpp
+++ Client.cpp
@@ -1,9 +1,121 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Client.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/12 19:45:21 by abenamar          #+#    #+#             */
+/*   Updated: 2024/08/26 12:33:18 by abenamar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "Client.hpp"
 
-std::string Client::getNickname() const throw() { return this->_nickname; }
+Client::Client(void) : connfd(-1) { return; }
 
-std::string Client::getClientname() const throw() { return this->_username; }
+Client::Client(int const connfd) : connfd(connfd), isMessageTooLong(false), input(), password(NULL), messages()
+{
+	this->input.reserve(Message::MAXSIZE);
 
-void Client::setNickname(std::string nickname) throw() { this->_nickname = nickname; }
+	return;
+}
 
-void Client::setClientname(std::string username) throw() { this->_username = username; }
+Client::Client(Client const & /* src */) : connfd(-1) { return; }
+
+Client::~Client(void) throw()
+{
+	delete password;
+
+	close(this->connfd);
+
+	return;
+}
+
+Client &Client::operator=(Client const & /* rhs */) throw() { return (*this); }
+
+bool Client::appendInput(char *const buffer)
+{
+	ssize_t rbcap, nread;
+
+	try
+	{
+		do
+		{
+			rbcap = Message::MAXSIZE - this->input.length();
+			nread = recv(connfd, buffer, rbcap, 0);
+
+			if (nread == -1)
+			{
+				if (errno == EAGAIN || errno == EWOULDBLOCK)
+					break;
+
+				throw RuntimeErrno("recv");
+			}
+
+			if (!nread)
+				return (false);
+
+			this->input.append(buffer, nread);
+			this->extractMessage(this->input.find_first_of(Message::CRLF, this->input.length() - nread));
+		} while (rbcap == nread);
+	}
+	catch (std::exception const &e)
+	{
+		throw std::runtime_error("Client::appendInput: " + std::string(e.what()));
+	}
+
+	return (true);
+}
+
+bool Client::hasMessage(void) const throw() { return (!this->messages.empty()); }
+
+Message const &Client::message(void)
+{
+	if (this->messages.empty())
+		throw std::runtime_error("Client::message: std::runtime_error: no message, `Client::hasMessage' must return true to call `Client::message'");
+
+	return (this->messages.front());
+}
+
+void Client::dropMessage(void) throw()
+{
+	if (this->messages.empty())
+		throw std::runtime_error("Client::dropMessage: std::runtime_error: nothing to drop, `Client::hasMessage' must return true to call `Client::dropMessage'");
+
+	this->messages.pop();
+
+	return;
+}
+
+void Client::extractMessage(std::size_t const crlfpos)
+{
+	if (crlfpos == std::string::npos)
+	{
+		if (this->input.length() == Message::MAXSIZE)
+			this->isMessageTooLong = true;
+
+		if (this->isMessageTooLong)
+			this->input.clear();
+
+		return;
+	}
+
+	try
+	{
+		if (!this->isMessageTooLong && 0 < crlfpos && crlfpos <= Message::MAXCHARS)
+			this->messages.push(Message::Builder()
+									.withInput(this->input.substr(0, crlfpos) + Message::CRLF)
+									.build());
+	}
+	catch (std::exception const &e)
+	{
+		throw std::runtime_error("Client::appendInput: " + std::string(e.what()));
+	}
+
+	this->isMessageTooLong = false;
+
+	this->input.erase(0, this->input.find_first_not_of(Message::CRLF, crlfpos));
+
+	return (this->extractMessage(this->input.find_first_of(Message::CRLF)));
+}
--- Client.hpp
+++ Client.hpp
@@ -1,35 +1,50 @@
-#ifndef CLIENT_HPP
-# define CLIENT_HPP
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Client.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/11 20:21:22 by abenamar          #+#    #+#             */
+/*   Updated: 2024/08/26 12:35:53 by abenamar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
+#ifndef __CLIENT_HPP__
+#define __CLIENT_HPP__
+
+#include <queue>
+#include <stdexcept>
 #include <string>
-
-#include "Client.hpp"
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include "Message.hpp"
+#include "RuntimeErrno.hpp"
 
 class Client
 {
-public :
-	Client() throw();
-	Client(std::string username, std::string nickname) throw();
-	Client(Client const &src) throw();
-	virtual ~Client() throw();	
-	Client& operator=(Client const &rhs) throw();
+public:
+	Client(int const connfd);
+	virtual ~Client(void) throw();
 
-	std::string getNickname() const throw();
-	std::string getClientname() const throw();
+	bool appendInput(char *const buffer);
+	bool hasMessage(void) const throw();
+	Message const &message(void);
+	void dropMessage(void) throw();
 
-	void setNickname(std::string nickname) throw();
-	void setClientname(std::string username) throw();
+private:
+	int const connfd;
+	bool isMessageTooLong;
+	std::string input, *password;
+	std::queue<Message> messages;
 
-	// void for now and we can throw exceptions later, or ints for error codes, to discuss
-	void authenticate(Client client) throw();
-	void joinChannel(Channel channel) throw();
-	void sendMessage(std::string const message) throw();
-	void receiveMessage() throw();
+	Client(void);
+	Client(Client const & /* src */);
 
-private :
-	int 		socket_fd;
-	std::string _username;
-	std::string _nickname;
+	Client &operator=(Client const & /* rhs */) throw();
+
+	void extractMessage(std::size_t const crlfpos);
 };
 
 #endif
