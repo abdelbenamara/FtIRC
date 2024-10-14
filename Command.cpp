a49201a895325d4fbe602093b69831ac88cc4575
Abdel Benamara <abdelb.contact@gmail.com>
2024-10-14 15:51:43 +0200

    Updated Command

--- Command.cpp
+++ Command.cpp
@@ -3,29 +3,33 @@
 /*                                                        :::      ::::::::   */
 /*   Command.cpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ejankovs <ejankovs@student.42.fr>          +#+  +:+       +#+        */
+/*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/21 21:21:33 by abenamar          #+#    #+#             */
-/*   Updated: 2024/10/09 21:10:43 by ejankovs         ###   ########.fr       */
+/*   Updated: 2024/10/14 15:49:13 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Command.hpp"
 
-std::map<std::string, void (*)(Client &, Server const &)> const Command::APPLY = Command::createApplyMap();
+std::map<std::string, void (*)(Client &, Server &)> const Command::APPLY = Command::createApplyMap();
 
-std::map<std::string, void (*)(Client &, Server const &)> Command::createApplyMap(void)
+std::map<std::string, void (*)(Client &, Server &)> Command::createApplyMap(void)
 {
-    std::map<std::string, void (*)(Client &, Server const &)> map;
+    std::map<std::string, void (*)(Client &, Server &)> map;
 
     map["PASS"] = &Command::pass;
     map["NICK"] = &Command::nick;
-    map["USER"] = &Command::pass;
+    map["USER"] = &Command::user;
 
     return (map);
 }
 
-void Command::pass(Client &client, Server const &server)
+Command::~Command(void) throw() { return; }
+
+Command &Command::operator=(Command const & /* rhs */) throw() { return (*this); }
+
+void Command::pass(Client &client, Server &server)
 {
     Message const &message = client.message();
     ssize_t nwrite = 0;
@@ -34,17 +38,13 @@ void Command::pass(Client &client, Server const &server)
     {
         if (message.getParameters().empty())
             nwrite = send(client.getSocket(), "461 PASS :Not enough parameters\r\n", 33, 0);
-        else if (client.isAuthorized())
-            nwrite = send(client.getSocket(), "462 :Unauthorized command (already registered)\r\n", 48, 0);
-        else if (message.getParameters().at(0) != server.getPassword())
-            nwrite = send(client.getSocket(), "464 :Password incorrect\r\n", 25, 0);
+        else if (message.getParameters().at(0) == server.getPassword())
+            client.setAuthorized(true);
 
         if (nwrite == -1)
             throw RuntimeErrno("send");
-
-        client.setAuthorized(true);
     }
-    catch (const std::exception &e)
+    catch (std::exception const &e)
     {
         throw std::runtime_error("Command::pass: " + std::string(e.what()));
     }
@@ -52,38 +52,42 @@ void Command::pass(Client &client, Server const &server)
     return;
 }
 
-void Command::nick(Client &client, Server const &server)
+void Command::nick(Client &client, Server &server)
 {
     Message const &message = client.message();
+    std::string const &nickname = message.getParameters().at(0);
     ssize_t nwrite = 0;
-	//bool const isNicknameFormat = std::find_if((message.getParameters().at(0)).begin(), (message.getParameters().at(0)).end(), Message::Builder::isNotInNicknameFormat) == (message.getParameters().at(0)).end();
-	
+    std::map<int, Client *const>::const_iterator it = server.getClientsBegin();
+
     try
     {
         if (message.getParameters().empty())
             nwrite = send(client.getSocket(), "431 :No nickname given\r\n", 24, 0);
-		// else if (isNicknameFormat)
-		// {
-		// 	std::string errorMessage = "432 " + message.getParameters().at(0) + " :Erroneous nickname\r\n";
-        //     nwrite = send(client.getSocket(), errorMessage.c_str(), errorMessage.length(), 0);
-		// }
-        else if (server.isNicknameInUse(message.getParameters().at(0)))
-		{
-			std::string errorMessage = "433 " + message.getParameters().at(0) + " :Nickname is already in use\r\n";
-            nwrite = send(client.getSocket(), errorMessage.c_str(), errorMessage.length(), 0);
-		}
+        else if (std::find_if(nickname.begin(), nickname.end(), Message::isNotInNicknameFormat) != nickname.end())
+            nwrite = send(client.getSocket(), ("432 " + nickname + " :Erroneous nickname\r\n").c_str(), nickname.length() + 26, 0);
         else
-        {
-            client.setNickname(message.getParameters().at(0));
-			
-			std::string nickMessage = "NICK :" + client.getNickname() + "\r\n";
-            nwrite = send(client.getSocket(), nickMessage.c_str(), nickMessage.length(), 0);
-        }
+            for (; it != server.getClientsEnd(); ++it)
+            {
+                if (it->second->getNickname() == nickname)
+                {
+                    nwrite = send(client.getSocket(), ("433 " + nickname + " :Nickname is already in use\r\n").c_str(), nickname.length() + 34, 0);
+
+                    break;
+                }
+            }
+
+        if (it == server.getClientsEnd())
+            client.setNickname(nickname);
+
+        if (client.isRegistered())
+            nwrite = send(client.getSocket(), ("001 " + client.getNickname() + " :Welcome to the ft_irc Network, " + client.getNickname() + "\r\n").c_str(), client.getNickname().length() * 2 + 39, 0);
+        else if (!client.isAuthorized() && client.isIdentified())
+            nwrite = send(client.getSocket(), "464 :Password incorrect\r\n", 25, 0);
 
         if (nwrite == -1)
             throw RuntimeErrno("send");
     }
-    catch (const std::exception &e)
+    catch (std::exception const &e)
     {
         throw std::runtime_error("Command::nick: " + std::string(e.what()));
     }
@@ -91,37 +95,34 @@ void Command::nick(Client &client, Server const &server)
     return;
 }
 
-void Command::user(Client &client, Server const &server)
+void Command::user(Client &client, Server &server)
 {
-	(void)server;
     Message const &message = client.message();
     ssize_t nwrite = 0;
+
+    (void)server;
 
     try
     {
         if (message.getParameters().size() < 4)
-            nwrite = send(client.getSocket(), "461 USER :Not enough parameters\r\n", 35, 0);
+            nwrite = send(client.getSocket(), "461 USER :Not enough parameters\r\n", 33, 0);
         else if (client.isRegistered())
-        {
-            nwrite = send(client.getSocket(), "462 :You may not reregister\r\n", 29, 0);
-        }
+            nwrite = send(client.getSocket(), "462 :Unauthorized command (already registered)\r\n", 48, 0);
         else
-        {
-            client.setUsername(message.getParameters().at(0));
-            client.setMode(message.getParameters().at(1));
-            client.setRealname(message.getParameters().at(3));
+            client.identify();
 
-            client.setRegistered(true);
-        }
+        if (client.isRegistered())
+            nwrite = send(client.getSocket(), ("001 " + client.getNickname() + " :Welcome to the ft_irc Network, " + client.getNickname() + "\r\n").c_str(), client.getNickname().length() * 2 + 39, 0);
+        else if (!client.isAuthorized() && !client.getNickname().empty())
+            nwrite = send(client.getSocket(), "464 :Password incorrect\r\n", 25, 0);
 
         if (nwrite == -1)
             throw RuntimeErrno("send");
     }
-    catch (const std::exception &e)
+    catch (std::exception const &e)
     {
         throw std::runtime_error("Command::user: " + std::string(e.what()));
     }
 
-	return ;
+    return;
 }
-
--- Command.hpp
+++ Command.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Command.hpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ejankovs <ejankovs@student.42.fr>          +#+  +:+       +#+        */
+/*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/21 21:05:45 by abenamar          #+#    #+#             */
-/*   Updated: 2024/10/09 20:16:38 by ejankovs         ###   ########.fr       */
+/*   Updated: 2024/10/14 15:46:12 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 
 #include <sys/socket.h>
 #include <sys/types.h>
+#include <algorithm>
 #include <map>
 #include <string>
 #include "Client.hpp"
@@ -28,7 +29,7 @@ class Command
 public:
     virtual ~Command(void) throw();
 
-    static std::map<std::string, void (*)(Client &, Server const &)> const APPLY;
+    static std::map<std::string, void (*)(Client &, Server &)> const APPLY;
 
 private:
     Command(void);
@@ -36,18 +37,17 @@ private:
 
     Command &operator=(Command const & /* rhs */) throw();
 
-    static std::map<std::string, void (*)(Client &, Server const &)> createApplyMap(void);
-    static void pass(Client &client, Server const &server);
-    static void nick(Client &client, Server const &server);
-    static void user(Client &client, Server const &server);
-
-	// static void quit(Client &client, Server const &server);
-	// static void join(Client &client, Server const &server);
-	// static void mode(Client &client, Server const &server);
-	// static void topic(Client &client, Server const &server);
-	// static void invite(Client &client, Server const &server);
-	// static void kick(Client &client, Server const &server);
-	// static void privmsg(Client &client, Server const &server);
+    static std::map<std::string, void (*)(Client &, Server &)> createApplyMap(void);
+    static void pass(Client &client, Server &server);
+    static void nick(Client &client, Server &server);
+    static void user(Client &client, Server &server);
+    // static void quit(Client &client, Server &server);
+    // static void privmsg(Client &client, Server &server);
+    // static void join(Client &client, Server &server);
+    // static void mode(Client &client, Server &server);
+    // static void topic(Client &client, Server &server);
+    // static void invite(Client &client, Server &server);
+    // static void kick(Client &client, Server &server);
 };
 
 #endif
