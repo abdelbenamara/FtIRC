02500ad4a85ec2bc7cae4e815a6dd1c1d20560c2
Abdel Benamara <abdelb.contact@gmail.com>
2024-10-28 20:53:03 +0100

    Updated RuntimeErrno

--- RuntimeErrno.hpp
+++ RuntimeErrno.hpp
@@ -6,7 +6,7 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/19 15:14:28 by abenamar          #+#    #+#             */
-/*   Updated: 2024/08/19 15:30:51 by abenamar         ###   ########.fr       */
+/*   Updated: 2024/10/24 23:18:31 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,8 @@
 class RuntimeErrno : public std::runtime_error
 {
 public:
-    explicit RuntimeErrno(std::string const &source) throw();
-    explicit RuntimeErrno(std::string const &parent, std::string const &source) throw();
+    explicit RuntimeErrno(std::string const &source);
+    explicit RuntimeErrno(std::string const &parent, std::string const &source);
 };
 
 #endif
